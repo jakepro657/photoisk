ebd43105ae32c91acf79875376ea9c304a05da3a
jakpro657 <jakepro657@gmail.com>
2025-01-02 09:56:22 +0900

    feat: clerk login

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,23 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Landing from './pages/Landing'
+import { ClerkProvider } from '@clerk/clerk-react'
+import { koKR } from "@clerk/localizations";
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Add your Clerk Publishable Key to the .env.local file')
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Landing />}></Route>
-      </Routes>
-    </BrowserRouter>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/" localization={koKR}>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Landing />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </ClerkProvider>
   </StrictMode>,
 )
